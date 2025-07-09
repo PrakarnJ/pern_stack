2f8c258602704b3488732665541bc3dae9b8a624
PrakarnJ <prakarn.jrs@gmail.com>
2025-07-09 16:52:21 +0700

    add backend

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,29 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Initialize Arcjet
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection, XSS, CSRF attacks, etc.
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      // block all bots except search engines
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        // see the full list at https://arcjet.com/bot-list
+      ],
+    }),
+    // rate limiting
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
---
+++ backend/server.js
@@ -0,0 +1,70 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")); // log the requests
+
+// apply arcjet rate-limit to all routes
+
+app.use((req, res, next) => {
+  try {
+    const decision = await aj.project(req, {
+      requested:1 // specifies that each request consumes 1 token
+    })
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+       res.status(429).json({error: "Too many Requests"}) 
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({error: "Bot access denied"})
+      } else {
+        res.status(403).json({error: "Forbidden"})
+      }
+      return
+    }
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected"})
+      return
+    }
+    next()
+  } catch (error) {
+    console.error("Error in Arcjet middleware:", error);
+    next(error)
+  }
+app.use("/api/products", productRoutes);
+
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB ", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
+});
